fa22d974041e1f7978ec5d03bff3045b5d0b25e7
Ramya <ramyapatil31@gmail.com>
2019-05-14 14:55:08 +0530

    QRScanner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { QRScanner } from '@ionic-native/qr-scanner/ngx';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -9,12 +10,32 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { Firebase } from '@ionic-native/firebase/ngx';
+
+const config = {
+  apiKey: "AIzaSyC58EfMYqI3qPXTVxAm9fGjjBWDgFR5Iww",
+  authDomain: "notification-5289d.firebaseapp.com",
+  databaseURL: "https://notification-5289d.firebaseio.com",
+  projectId: "notification-5289d",
+  storageBucket: "notification-5289d.appspot.com",
+  messagingSenderId: "531419110428"
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+     BrowserModule,
+     IonicModule.forRoot(),
+     AppRoutingModule,
+     AngularFireModule.initializeApp(config),
+     AngularFirestoreModule],
   providers: [
     StatusBar,
+    Firebase,
+    QRScanner,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(private qrScanner: QRScanner) { }
+
+  scanner(){
+    // Optionally request the permission early
+this.qrScanner.prepare()
+.then((status: QRScannerStatus) => {
+   if (status.authorized) {
+     // camera permission was granted
+
+
+     // start scanning
+     let scanSub = this.qrScanner.scan().subscribe((text: string) => {
+       console.log('Scanned something', text);
+
+       this.qrScanner.hide(); // hide camera preview
+       scanSub.unsubscribe(); // stop scanning
+     });
+
+   } else if (status.denied) {
+     // camera permission was permanently denied
+     // you must use QRScanner.openSettings() method to guide the user to the settings page
+     // then they can grant the permission from there
+   } else {
+     // permission was denied, but not permanently. You can ask for permission again at a later time.
+   }
+})
+.catch((e: any) => console.log('Error is', e));
+  }
 
 }
